76a72d072feb2ad21394e5ff455d2266747b25bd
Sam <stberg3@gmail.com>
2020-07-25 14:19:09 -0500

    Working skeleton!

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var socketServer = require('socket.io');
 
 var indexRouter = require('./routes/index');
-
 var app = express();
+var http = require('http').createServer(app); 
+var io = require('socket.io')(http);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -36,4 +33,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
